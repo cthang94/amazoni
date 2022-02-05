092ea7b8d0d730b9801e07f478b90639e1862a6f
cthang94 <calvin.thang02@qmail.cuny.edu>
2022-02-05 13:39:05 -0500

    added delete button

--- src/Product.jsx
+++ src/Product.jsx
@@ -6,45 +6,62 @@ class Product extends Component {
   };
   render() {
     return (
-      <div className="row d-flex align-items-stretch">
-        <div className="card m-3">
-          <div className="card-body pt-3">
-            <div className="text-muted">Book ID #{this.state.product.id}</div>
-            <h2 className="pt-3 border-top text-center text-decoration-underline card-title">
+      <div className="row d-flex align-items-stretch m-auto">
+        <div className="card p-3 m-3">
+          <div className="card-body">
+            <div className="text-muted text-start">
+              Book ID <strong>#{this.state.product.id}</strong>
+              <span
+                className="pull-right hand-icon"
+                onClick={() => {
+                  this.props.onDelete(this.state.product);
+                }}
+              >
+                <i className="fa fa-times"></i>
+              </span>
+            </div>
+            <h2 className="p-3 border-top text-center text-decoration-underline card-title">
               {this.state.product.productName}
             </h2>
             <img
-              className="img-fluid"
+              className="img-fluid border\"
               src={this.state.product.img}
               alt="Textbook"
               height="300"
               width="250"
             ></img>
-            <div className="text-start">
-              <p className="">Price: ${this.state.product.price}</p>
-            </div>
+            <p class="">{this.state.product.description}</p>
           </div>
           {/* card body ends here */}
           <div className="card-footer">
-            <div className=" mt-2 pt-2 float-start">
+            <div className="float-start m-1 p-4">
               {this.state.product.quantity}
               <span className="badge"></span>
-              <div className="btn-group">
+              <div className="btn-group m-2">
                 <button
                   className="btn btn-outline-success"
-                  onClick={this.props.onIncrease}
+                  onClick={() => {
+                    this.props.onIncrease(this.state.product, 10);
+                  }}
                 >
                   +
                 </button>
                 <button
                   className="btn btn-outline-success"
-                  onClick={this.props.onDecrease}
+                  onClick={() => {
+                    this.props.onDecrease(this.state.product, 0);
+                  }}
                 >
                   -
                 </button>
               </div>
             </div>
-            <div className="float-end">{this.props.children}</div>
+
+            <div className="float-end ">
+              <p className="m-2">Price: ${this.state.product.price}</p>
+
+              <div className="m-2">{this.props.children}</div>
+            </div>
           </div>
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 
 import App from "./App";
-import NavBar from "./NavBar";
 import "./index.css";
+import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
