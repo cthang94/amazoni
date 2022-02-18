edf51dc4b6bcf800504088ea3626d3667fa48620
cthang94 <calvin.thang02@qmail.cuny.edu>
2022-02-17 19:10:55 -0500

    changed

--- src/Home.jsx
+++ src/Home.jsx
@@ -17,7 +17,7 @@ class Home extends Component {
           return (
             <Product
               key={this.state.products.id}
-              product={this.state.products}
+              product={this.state.products.product}
               onIncrease={this.quantityIncrease}
               onDecrease={this.quantityDecrease}
               onDelete={this.handleDelete}
